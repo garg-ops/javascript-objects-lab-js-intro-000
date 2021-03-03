88dc26ec4888f149151d854c0202766e2b9dfd74
Nilesh Garg <nileshgarg0023@gmail.com>
2021-03-03 15:18:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,9 +18,6 @@ function deleteFromObjectByKey(object, key) {
 
 }
 
-function destructivelyDeleteFromObjectByKey(object, key) {
-
-}
 
 function destructivelyDeleteFromObjectByKey(object, key) {
   delete.object[key]
